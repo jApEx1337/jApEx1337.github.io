2eea683a895240d96a8e3a08d54f337e68dd2ec6
jApEx1337 <65730583+jApEx1337@users.noreply.github.com>
2021-01-31 23:11:38 +0300

    Create index.php

---
+++ index.php
@@ -0,0 +1,70 @@
+<html>
+<head>
+<meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
+<title> lynph </title>
+
+<link rel="stylesheet" href="style/style.css">
+
+  <style>
+        video {
+  width: 100%;
+  height: auto;
+  max-height: 100%;
+  pointer-events: none;
+}
+html, body{
+    padding: 0px;
+    margin: 0px;
+    overflow: hidden;
+    background-color: black;
+}
+.mainf{
+    position: absolute;
+    width: 100%;
+    height: 100%;
+    
+    text-align: center;
+    
+}
+.dark{
+    position: absolute;
+    width: 100%;
+    height: 100%;
+    background: rgba(0,0,0,0.5);
+}
+h1,a{
+    color: white;
+    text-decoration: none;
+}
+#newinfo, #newinfoq{
+    display: none;
+}
+    </style>
+
+</head><body onclick="myFunction()">
+    <div class="dark">
+        
+    </div>
+<video id="trap" preload="auto" autoplay="true" loop="true" muted="">
+            <source src="https://lynph.pw/You're so fucking precious.mp4" type="video/mp4">
+        </video>
+<div id="center">
+<div id="main" style="opacity: 1.1; visibility: visible; display: block;">
+<h1 id="plbtn">Click...</h1>
+<h1 id="newinfo">Weebs.club</h1>
+<center><p id="newinfoq"><a href="https://weebs.club" target="_blank">forum</a></p></center>
+</div>
+</div>
+<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
+<script>
+function myFunction() {
+    var vid = document.getElementById("trap");
+    vid.volume = 0.5;
+    $("video").prop('muted', false); //unmute
+    document.getElementById("plbtn").style.display = "none";
+    document.getElementById("newinfo").style.display = "block";
+    document.getElementById("newinfoq").style.display = "block";
+}
+
+</script>
+<div at-magnifier-wrapper=""><div class="at-theme-light"><div class="at-base notranslate" translate="no"><div class="Z1-AJ" style="top: 0px; left: 0px;"></div></div></div></div></body></html>
